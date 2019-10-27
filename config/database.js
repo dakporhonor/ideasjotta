3321e5fbaa2d97035e8c908674f8a9469cf0fff4
Dan Akporhonor <d.akporhonor@gmail.com>
2019-10-27 14:41:37 +0100

    Update database

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 if(process.env.NODE_ENV === 'production') {
   module.exports = {
-    mongoURL: 'mongodb+srv://dan:1234@vidprod-o2ut2.mongodb.net/test?retryWrites=true&w=majority'
+    mongoURL: 'mongodb+srv://dan:12345@vidprod-o2ut2.mongodb.net/test?retryWrites=true&w=majority'
   }
 }else {
   module.exports = {
